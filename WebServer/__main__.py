755843a43af3665adecd4e45c21a5d554b3a0c3c
Junhua <wenjh1998@gmail.com>
2021-05-07 23:16:38 +0800

    Add Stored XSS Vulnerability

--- WebServer/__main__.py
+++ WebServer/__main__.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import datetime
 from flask import Flask, render_template, redirect, request, flash
 
 # Initialise the application
@@ -13,9 +14,17 @@ BLANK_ERROR_MSG = 'Please do not leave any fields blank!'
 CREATE_QUERY = 'CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)'
 LOGIN_QUERY = 'SELECT * FROM users WHERE username = \'{}\' AND password = \'{}\''
 
+# Prepared statements below to prevent SQLi
+# Query for plant DB
 OTHER_DB_PATH = os.path.join('data', 'data2.db')
-CREATE_PLANT_QUERY = 'CREATE TABLE IF NOT EXISTS plants (name TEXT)'
 SEARCH_QUERY = 'SELECT * FROM plants p WHERE p.name LIKE \'%\' || ? || \'%\''
+CREATE_PLANT_QUERY = 'CREATE TABLE IF NOT EXISTS plants (name TEXT)'
+
+# Query for posting
+CREATE_POST_QUERY = 'CREATE TABLE IF NOT EXISTS posts (content TEXT, author TEXT, time TIMESTAMP)'
+TRUNCATE_POSTS = 'DELETE FROM posts'
+GET_ALL_POSTS = 'SELECT * FROM posts'
+INSERT_POST = 'INSERT INTO posts VALUES(?, ?, ?)'
 
 
 # Database methods
@@ -33,6 +42,7 @@ def create_db() -> None:
         # Create the table if it does not exist
         cur = db.cursor()
         cur.execute(CREATE_PLANT_QUERY)
+        cur.execute(CREATE_POST_QUERY)
 
 
 @app.route('/')
@@ -40,9 +50,8 @@ def index():
     """The index page for the website"""
     return render_template('index.html')
 
+
 # Reflected XSS Methods
-
-
 @app.route('/rxss', methods=['POST', 'GET'])
 def reflected_xss():
     """Page to showcase Reflected XSS"""
@@ -65,12 +74,11 @@ def reflected_xss():
         cur.execute(CREATE_PLANT_QUERY)
 
         # Search for the plant
-
         # Use prepared statements here to prevent SQLi
         cur.execute(SEARCH_QUERY, (data['query'],))
         results = tuple(map(lambda x: x[0], cur.fetchall()))
 
-    return render_template('rxss.html', results=results, query = data['query'])
+    return render_template('rxss.html', results=results, query=data['query'])
 
 
 # CSRF Methods
@@ -81,10 +89,43 @@ def csrf():
 
 
 # Stored XSS Methods
-@app.route('/sxss')
+@app.route('/sxss', methods=['POST', 'GET'])
 def stored_xss():
     """Page to showcase Stored XSS"""
-    return render_template('sxss.html')
+
+    # Get all posts
+    with sqlite3.connect(OTHER_DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute(GET_ALL_POSTS)
+        posts = cur.fetchall()
+
+    # Get request
+    if request.method == 'GET':
+        return render_template('sxss.html', posts=posts)
+
+    # Get form data
+    data = request.form
+    print(data)
+
+    # Check if user wants to clear data
+    if data.get('clear', None):
+        with sqlite3.connect(OTHER_DB_PATH) as db:
+            cur = db.cursor()
+            cur.execute(TRUNCATE_POSTS)
+        flash('Posts cleared')
+        return redirect('/sxss')
+
+    # Check for empty data
+    if len(data) == 0 or '' in data.values():
+        flash('Please do not submit a blank form')
+        return redirect('/sxss')
+
+    # Add the posts to the table
+    with sqlite3.connect(OTHER_DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute(INSERT_POST, (data.get('post'), data.get('author'), datetime.datetime.now()))
+    flash('Post added')
+    return redirect('/sxss')
 
 
 # SQLi Methods
