227f12fdafbeb9956e1ea6734d163925dbcbfde1
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2021-06-02 23:15:40 +0800

    add manager user, have admin:password

--- WebServer/util.py
+++ WebServer/util.py
@@ -1,7 +1,7 @@
-initial_users = [('admin', 'passwordthatissupersecret'), ('user1', 'justaplebuserlmao')]
+initial_users = [('admin', 'password'), ('user1', 'justaplebuserlmao'), ('manager', 'passwordthatissupersecret')]
 
 initial_notes = [
-    ('Administration Note', 'Please do not try to hack us, we are proven to be super secure!', 'admin', False),
-    ('Dammit', 'We might get hacked and get our secret key exposed, which is SUPERSECRETKEY1012', 'admin', True),
+    ('Administration Note', 'Please do not try to hack us, we are proven to be super secure!', 'manager', False),
+    ('Dammit', 'We might get hacked and get our secret key exposed, which is SUPERSECRETKEY1012', 'manager', True),
     ('Wow', 'This app looks so fun! I wonder if there is any secrets lying within...', 'user1', False),
 ]
